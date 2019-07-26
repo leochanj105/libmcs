leochanj105/libmcs

commit 858be17ffb19dd7e9e96e967a11f942c40d9ef70

hash functions: use reentrant stat functions
_stat64 and _fstat64 are not exported from Cygwin.  Use the
reentrant analogues, like everywhere else.

Signed-off-by: Corinna Vinschen <corinna-cygwin@cygwin.com>

newlib/libc/search/hash.c
@@ -140,9 +140,9 @@ __hash_open (const char *file,
 	new_table = 0;
 	if (!file || (flags & O_TRUNC) ||
 #ifdef __USE_INTERNAL_STAT64
-	    (_stat64(file, &statbuf) && (errno == ENOENT))) {
+	    (_stat64_r(_REENT, file, &statbuf) && (errno == ENOENT))) {
 #else
-	    (stat(file, &statbuf) && (errno == ENOENT))) {
+	    (_stat_r(_REENT, file, &statbuf) && (errno == ENOENT))) {
 #endif
 		if (errno == ENOENT)
 			errno = 0; /* Just in case someone looks at errno */
@@ -156,9 +156,9 @@ __hash_open (const char *file,
 		   a new .db file, then reinitialize the database */
 		if ((flags & O_CREAT) &&
 #ifdef __USE_INTERNAL_STAT64
-		     _fstat64(hashp->fp, &statbuf) == 0 && statbuf.st_size == 0)
+		     _fstat64_r(_REENT, hashp->fp, &statbuf) == 0 && statbuf.st_size == 0)
 #else
-		     fstat(hashp->fp, &statbuf) == 0 && statbuf.st_size == 0)
+		     _fstat_r(_REENT, hashp->fp, &statbuf) == 0 && statbuf.st_size == 0)
 #endif
 			new_table = 1;
 
@@ -341,9 +341,9 @@ init_hash(hashp, file, info)
 	/* Fix bucket size to be optimal for file system */
 	if (file != NULL) {
 #ifdef __USE_INTERNAL_STAT64
-		if (_stat64(file, &statbuf))
+		if (_stat64_r(_REENT, file, &statbuf))
 #else
-		if (stat(file, &statbuf))
+		if (_stat_r(_REENT, file, &statbuf))
 #endif
 			return (NULL);
 		hashp->BSIZE = MIN(statbuf.st_blksize, MAX_BSIZE);

commit 37d4ad450e4bfdaa1938d26280d99be9d184c82e

hash.c: #include <reent.h>
This is needed for the prototypes of _stat64 and _fstat64 on Cygwin.

Fixes: commit 858be17f "hash functions: use reentrant stat functions".

newlib/libc/search/hash.c
@@ -43,6 +43,7 @@ static char sccsid[] = "@(#)hash.c	8.9 (Berkeley) 6/16/94";
 
 #include <sys/stat.h>
 
+#include <reent.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>