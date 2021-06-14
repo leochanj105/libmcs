leochanj105/libmcs

commit 116006e6730df1854852629b51e04cd9a6ff0c82

ceil/f, floor/f: Fix inexact when zero

libm/mathd/floord.c
@@ -66,11 +66,15 @@ double floor(double x)
 
     if (j0 < 20) {
         if (j0 < 0) {  /* raise inexact if x != 0 */
+            if ((i0 | i1) == 0) {
+                return x;
+            }
+
             (void) __raise_inexact(x);
-            
+
             if (i0 >= 0) {
                 i0 = i1 = 0;
-            } else if (((i0 & 0x7fffffff) | i1) != 0) {
+            } else {
                 i0 = (int32_t)0xbff00000;
                 i1 = 0;
             }

libm/mathd/ceild.c
@@ -24,12 +24,16 @@ double ceil(double x)
 
     if (j0 < 20) {
         if (j0 < 0) {  /* raise inexact if x != 0 */
+            if ((i0 | i1) == 0) {
+                return x;
+            }
+
             (void) __raise_inexact(x);
             
             if (i0 < 0) { /* return 0*sign(x) if |x|<1 */
                 i0 = (int32_t)0x80000000;
                 i1 = 0;
-            } else if ((i0 | i1) != 0) {
+            } else {
                 i0 = 0x3ff00000;
                 i1 = 0;
             }

libm/mathf/ceilf.c
@@ -15,11 +15,15 @@ float ceilf(float x)
 
     if (j0 < 23) {
         if (j0 < 0) {  /* raise inexact if x != 0 */
+            if (FLT_UWORD_IS_ZERO(ix)) {
+                return x;
+            }
+
             (void) __raise_inexactf(x); /* raise inexact flag */
-            
+
             if (i0 < 0) {
                 i0 = (int32_t)0x80000000;
-            } else if (!FLT_UWORD_IS_ZERO(ix)) {
+            } else {
                 i0 = 0x3f800000;
             }
         } else {

libm/mathf/floorf.c
@@ -24,11 +24,15 @@ float floorf(float x)
 
     if (j0 < 23) {
         if (j0 < 0) {  /* raise inexact if x != 0 */
+            if (FLT_UWORD_IS_ZERO(ix)) {
+                return x;
+            }
+
             (void) __raise_inexactf(x);
 
             if (i0 >= 0) {
                 i0 = 0;
-            } else if (!FLT_UWORD_IS_ZERO(ix)) {
+            } else {
                 i0 = (int32_t)0xbf800000;
             }
         } else {

commit 657eba8221c256acd0a9070aa7a7869569fde850

floor: Fix error introduced in 116006e6 for -0.0 input

libm/mathd/floord.c
@@ -66,7 +66,7 @@ double floor(double x)
 
     if (_j0 < 20) {
         if (_j0 < 0) {  /* raise inexact if x != 0 */
-            if ((_i0 | _i1) == 0) {
+            if (((_i0 & 0x7fffffff) | _i1) == 0) {
                 return x;
             }
 